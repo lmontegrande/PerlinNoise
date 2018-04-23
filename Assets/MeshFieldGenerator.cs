693704427e997b3e834e9c8c6ea83320494f2c16
MSI\lmont <lmontegrande91@gmail.com>
2018-04-22 20:53:36 -0800

    - Perlin Noise Demo with Unity's Procedural Mesh

---
+++ Assets/MeshFieldGenerator.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MeshFieldGenerator : MonoBehaviour {
+    public Button animateButton;
+    public Button stopButton;
+    public Slider animationSpeedSlider;
+    public Slider xSlider;
+    public Slider zSlider;
+    public Slider sizeOffsetSlider;
+    public Slider noiseScaleSlider;
+    public Slider noiseSizeSlider;
+    public Slider deltaTSlider;
+    public GameObject cubeMakerGameObject;
+    public bool isNoisy = true;
+    
+    private int xDimension = 1;
+    private int zDimension = 1;
+    private float cubeStartingSize = 1;
+
+    private List<GameObject> cubeField = new List<GameObject>();
+
+    public void Start()
+    {
+        animateButton.onClick.AddListener(Animate);
+        stopButton.onClick.AddListener(StopAnimate);
+        xSlider.onValueChanged.AddListener(GenerateField);
+        zSlider.onValueChanged.AddListener(GenerateField);
+        sizeOffsetSlider.onValueChanged.AddListener(GenerateField);
+        noiseScaleSlider.onValueChanged.AddListener(GenerateField);
+        noiseSizeSlider.onValueChanged.AddListener(GenerateField);
+        deltaTSlider.onValueChanged.AddListener(GenerateField);
+
+        GenerateField();
+    }
+
+    private void GenerateField(float val)
+    {
+        GenerateField();
+    }
+
+    private void GenerateField() {
+
+        SetValues();
+        ClearField();
+
+        for(int x=0; x<xDimension; x++)
+        {
+            for(int z=0; z<zDimension; z++)
+            {
+                Vector3 startingLoc = (new Vector3((float)x-(xDimension/2), 0, (float)z-(zDimension/2))) * cubeStartingSize;
+                GameObject cubeInstance = GameObject.Instantiate(cubeMakerGameObject, startingLoc, Quaternion.identity);
+                cubeInstance.GetComponent<CubeMaker>().size = Vector3.one * (cubeStartingSize * sizeOffsetSlider.value);
+                
+                if (isNoisy)
+                {
+                    Vector2 perlinPosition = new Vector2((x * noiseScaleSlider.value) + deltaTSlider.value, (z * noiseScaleSlider.value));
+                    float perlinHeight = Mathf.PerlinNoise(perlinPosition.x, perlinPosition.y);
+                    cubeInstance.GetComponent<CubeMaker>().size.y = perlinHeight * noiseSizeSlider.value;
+                }
+
+                cubeField.Add(cubeInstance);
+            }
+        }
+    }
+
+    private void ClearField()
+    {
+        while (cubeField.Count > 0)
+        {
+            GameObject cube = cubeField[0];
+            cubeField.RemoveAt(0);
+            Destroy(cube);
+        }
+    }
+
+    private void SetValues()
+    {
+        xDimension = (int) xSlider.value;
+        zDimension = (int) zSlider.value;
+    }
+
+    private Coroutine animateCoroutine;
+    private void Animate()
+    {
+        StopAnimate();
+        animateCoroutine = StartCoroutine(AnimateCoroutine());
+    }
+
+    private void StopAnimate()
+    {
+        if (animateCoroutine != null)
+            StopCoroutine(animateCoroutine);
+    }
+
+    private IEnumerator AnimateCoroutine()
+    {
+        GenerateField();
+        deltaTSlider.value = 0f;
+        while(true)
+        {
+            if (deltaTSlider.value >= deltaTSlider.maxValue)
+                deltaTSlider.value = 0f;
+
+            deltaTSlider.value += animationSpeedSlider.value * Time.deltaTime;
+            yield return null;
+        }
+    }
+}
---
+++ Assets/Scripts/CubeMaker.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshRenderer))]
+[RequireComponent(typeof(MeshFilter))]
+public class CubeMaker : MonoBehaviour {
+    public Vector3 size = Vector3.one;
+    
+    private void Start()
+    {
+        MeshFilter meshFilter = this.GetComponent<MeshFilter>();
+        MeshCreator mc = new MeshCreator(); // one submesh for each face
+
+        Vector3 cubeSize = size * 0.5f;
+
+        // top of the cube
+        // t0 is top left point
+        Vector3 t0 = new Vector3(cubeSize.x, cubeSize.y, -cubeSize.z);
+        Vector3 t1 = new Vector3(-cubeSize.x, cubeSize.y, -cubeSize.z);
+        Vector3 t2 = new Vector3(-cubeSize.x, cubeSize.y, cubeSize.z);
+        Vector3 t3 = new Vector3(cubeSize.x, cubeSize.y, cubeSize.z);
+
+        // bottom of the cube
+        Vector3 b0 = new Vector3(cubeSize.x, -cubeSize.y, -cubeSize.z);
+        Vector3 b1 = new Vector3(-cubeSize.x, -cubeSize.y, -cubeSize.z);
+        Vector3 b2 = new Vector3(-cubeSize.x, -cubeSize.y, cubeSize.z);
+        Vector3 b3 = new Vector3(cubeSize.x, -cubeSize.y, cubeSize.z);
+
+        // Top square
+        mc.BuildTriangle(t0, t1, t2);
+        mc.BuildTriangle(t0, t2, t3);
+
+        // Bottom square
+        mc.BuildTriangle(b2, b1, b0);
+        mc.BuildTriangle(b3, b2, b0);
+
+        // Back square
+        mc.BuildTriangle(b0, t1, t0);
+        mc.BuildTriangle(b0, b1, t1);
+
+        mc.BuildTriangle(b1, t2, t1);
+        mc.BuildTriangle(b1, b2, t2);
+
+        mc.BuildTriangle(b2, t3, t2);
+        mc.BuildTriangle(b2, b3, t3);
+
+        mc.BuildTriangle(b3, t0, t3);
+        mc.BuildTriangle(b3, b0, t0);
+
+        meshFilter.mesh = mc.CreateMesh();
+    }
+}
